3a57969c6a2d5821366c1e5e36b728dc7a6f2569
Iva <60818270+iva7777@users.noreply.github.com>
2022-03-22 10:52:04 +0200

    DbContext

--- Hotel_Reservations_Manager/Data/ApplicationDbContext.cs
+++ Hotel_Reservations_Manager/Data/ApplicationDbContext.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System;
+using Hotel_Reservations_Manager.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +11,27 @@ namespace Hotel_Reservations_Manager.Data
             : base(options)
         {
         }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<ClientReservation> ClientReservations { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ClientReservation>()
+                .HasKey(cr => new { cr.ClientId, cr.ReservationId });
+            builder.Entity<ClientReservation>()
+                .HasOne(cr => cr.Client)
+                .WithMany(c => c.ClientReservations)
+                .HasForeignKey(cr => cr.ClientId);
+            builder.Entity<ClientReservation>()
+                .HasOne(cr => cr.Reservation)
+                .WithMany(r => r.ClientReservations)
+                .HasForeignKey(cr => cr.ReservationId);
+        }
     }
 }
+
